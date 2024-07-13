b0f2280bf2aa89f7b6a417854e71f4ea4f26a970
achille.vlke <achillevanlunen@gmail.com>
2024-07-13 16:44:52 +0200

    added comments and overall polish

--- main.py
+++ main.py
@@ -7,22 +7,24 @@ def main():
 
     print(get_book_text(book_path))     # print the book
     print()
-    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")       # print words count
     print()
     for item in sorted_characters:
         print(f"The '{item['letter']}' character was found {item['count']} times")
+    print()
+    print("--- End report ---")
     
 def sort_num_characters(dict):
-    char_list = [{"letter": letter, "count": count} for letter, count in dict.items()]
-    sorted_num = sorted(char_list, key=lambda x: x["count"], reverse=True)
+    char_list = [{"letter": letter, "count": count} for letter, count in dict.items()] # creates a list of dictionaries
+    sorted_num = sorted(char_list, key=lambda x: x["count"], reverse=True) # sorts the list using the value of each dictionary, in descending order
     return sorted_num
 
 def get_num_characters(text):
     lowered_text = text.lower()
     char_count = {}
     for char in lowered_text:
-        if char.isalpha():
+        if char.isalpha(): # filters non alphabet words
             if char in char_count:
                 char_count[char] += 1
             else:
