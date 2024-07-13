fe3a0f312a9286017c73eae370d5fd0d53b0d2f4
achille.vlke <achillevanlunen@gmail.com>
2024-07-13 13:30:03 +0200

    added letters count

--- main.py
+++ main.py
@@ -2,9 +2,28 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    num_characters = get_num_characters(text)
 
     print(get_book_text(book_path))     # print the book
+    print()
     print(f"{num_words} words found in the document")       # print words count
+    print()
+    sorted_characters = dict(sorted(num_characters.items()))        # sorts the characters in the dictionary
+    print("Number of each character in the text:")
+    for char, count in sorted_characters.items():
+        print(f"{char}: {count}")       # print number of each character
+
+
+def get_num_characters(text):
+    lowered_text = text.lower()
+    char_count = {}
+    for char in lowered_text:
+        if char.isalpha():
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
+    return char_count
 
 
 def get_num_words(text):
@@ -16,5 +35,5 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-if __name__ == "__main__":
+if __name__ == "__main__":      # ensure execution only if called
     main()
