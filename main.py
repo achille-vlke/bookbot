fe634846519a8e8ca9ad212c6ac252017b8d4638
achille.vlke <achillevanlunen@gmail.com>
2024-07-13 16:34:17 +0200

    added sorted num count

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_characters = get_num_characters(text)
+    sorted_characters = sort_num_characters(num_characters)
 
     print(get_book_text(book_path))     # print the book
     print()
+    print("--- Begin report of books/frankenstein.txt ---")
     print(f"{num_words} words found in the document")       # print words count
     print()
-    sorted_characters = dict(sorted(num_characters.items()))        # sorts the characters in the dictionary
-    print("Number of each character in the text:")
-    for char, count in sorted_characters.items():
-        print(f"{char}: {count}")       # print number of each character
-
+    for item in sorted_characters:
+        print(f"The '{item['letter']}' character was found {item['count']} times")
+    
+def sort_num_characters(dict):
+    char_list = [{"letter": letter, "count": count} for letter, count in dict.items()]
+    sorted_num = sorted(char_list, key=lambda x: x["count"], reverse=True)
+    return sorted_num
 
 def get_num_characters(text):
     lowered_text = text.lower()
